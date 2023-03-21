2d98791430aa64b9debb6a5c3aab536524623c1d
luciasupova <luciasupovaa@gmail.com>
2023-03-21 14:17:15 +0100

    Locations

--- washworld-app/src/components/LocationList.tsx
+++ washworld-app/src/components/LocationList.tsx
@@ -1,4 +1,4 @@
-import { List, ListItem, ListItemText } from '@mui/material';
+import { List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
 import { Location } from '../types';
 
 type Props = {
@@ -7,19 +7,48 @@ type Props = {
 
 export default function LocationList({ locations }: Props) {
   return (
-<List>
-  {locations && locations.length > 0 ? (
-    locations.map((location) => (
-      <ListItem key={location.id} button>
-        <ListItemText primary={location.name} />
-      </ListItem>
-    ))
-  ) : (
-    <ListItem>
-      <ListItemText primary="No locations found" />
-    </ListItem>
-  )}
-</List>
-
+    <List>
+      {locations && locations.length > 0 ? (
+        locations.map((location) => (
+          <ListItem
+            key={location.id}
+            button
+            sx={{
+              border: '2px solid black',
+              backgroundColor: '#06C167',
+              color: 'white',
+              borderRadius: '3px',
+              marginTop: '0.5rem',
+              padding: '1rem',
+              ':hover': {
+                backgroundColor: '#16a25e',
+              },
+            }}
+          >
+            <ListItemIcon>
+              <span
+                style={{
+                  display: 'block',
+                  width: '10px',
+                  height: '10px',
+                  borderRadius: '50%',
+                  marginLeft: '15px',
+                  backgroundColor: location.status === 'maintenance' ? 'orange' : 'white',
+                }}
+              />
+            </ListItemIcon>
+            
+            <ListItemText
+              primary={<span style={{ fontWeight: 'bold' }}>{location.name}</span>}
+              secondary={location.status}
+            />
+          </ListItem>
+        ))
+      ) : (
+        <ListItem>
+          <ListItemText primary="No locations found" />
+        </ListItem>
+      )}
+    </List>
   );
 }
