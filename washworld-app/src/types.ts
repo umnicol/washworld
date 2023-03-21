5e4194c1303f057701a32bcb6aa9207c2715ae22
umnicol <nicoltomms@gmail.com>
2023-03-21 14:55:35 +0100

    1

--- washworld-app/src/types.ts
+++ washworld-app/src/types.ts
@@ -3,4 +3,13 @@ export interface Location {
     name: string;
     status: string;
   }
+
+  export interface Products {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    program: number;
+  }
+  
   
