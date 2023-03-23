be5333d86fd904cf350011b38c96578359e1453d
Lauraatii <tihanova.laura@gmail.com>
2023-03-23 17:02:36 +0100

    timer updated

--- washworld-app/src/pages/timer.tsx
+++ washworld-app/src/pages/timer.tsx
@@ -7,12 +7,12 @@ type CarWashTimerProps = {
 };
 
 const CarWashTimer = ({ ProductId, LocationId }: CarWashTimerProps) => {
-  const [seconds, setSeconds] = useState(0);
+  const [seconds, setSeconds] = useState(900); // 15 minutes in seconds
   const [timerId, setTimerId] = useState<NodeJS.Timeout | null>(null);
 
   const startTimer = () => {
     const id = setInterval(() => {
-      setSeconds((prevSeconds) => prevSeconds + 1);
+      setSeconds((prevSeconds) => prevSeconds - 1);
     }, 1000);
     setTimerId(id);
   };
@@ -21,7 +21,6 @@ const CarWashTimer = ({ ProductId, LocationId }: CarWashTimerProps) => {
     if (timerId) {
       clearInterval(timerId);
       setTimerId(null);
-      // send POST request to stop the car wash program
       try {
         await fetch(`https://b46f027d-3a5f-4de6-9075-5e861759e531.mock.pstmn.io/:location/start/:program
         `, {
@@ -30,19 +29,19 @@ const CarWashTimer = ({ ProductId, LocationId }: CarWashTimerProps) => {
       } catch (error) {
         console.log(error);
       }
-      setSeconds(0);
+      setSeconds(900);
     }
   };
 
-  const formattedTime = new Date(seconds * 1000).toISOString().substr(11, 8);
+  const formattedTime = new Date(seconds * 1000).toISOString().substr(14, 5); // format minutes and seconds only
 
     return (
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
           <Typography variant="h6" sx={{ marginBottom: '1rem'}}>
-            {`Car wash timer for product ${ProductId} at location ${LocationId}`}
+            {`Your car wash program ${ProductId} at location ${LocationId}. Time: 15 min`}
           </Typography>
           <Typography variant="h4" sx={{ marginBottom: '2rem' }}>{formattedTime}</Typography>
-          <Button variant="contained" color="primary" sx={{ backgroundColor: 'green', '&:hover': { backgroundColor: 'green' } }} onClick={startTimer} disabled={timerId !== null}>
+          <Button variant="contained" color="primary" sx={{ backgroundColor: '#06C167', '&:hover': { backgroundColor: '#06C168' } }} onClick={startTimer} disabled={timerId !== null}>
             Start Wash
           </Button>
           <Button variant="contained" color="secondary" sx={{ marginTop:'0.5rem', backgroundColor: 'red', '&:hover': { backgroundColor: 'red' } }} onClick={stopTimer} disabled={timerId === null}>
